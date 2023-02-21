e6be7d9017fbecf72053fba5d18ad9b58e13bb0b
Adijnnuuy <96059201+Adi-SOUL@users.noreply.github.com>
2023-02-22 00:05:27 +0800

    Add files via upload

--- tools/tools.py
+++ tools/tools.py
@@ -1,38 +1,31 @@
 import re
 import threading
-from typing import Tuple
-
 from PIL import Image
+
+Image.MAX_IMAGE_PIXELS = None
+from typing import Tuple
 
 
 def get_img(img_file: str, ratio: float, img_pil: Image = None) -> Tuple[Image.Image, Image.Image, str]:
 	if img_pil is None:
-		img = Image.open(img_file).convert('RGB')
+		img = Image.open(img_file)
 	else:
 		img = img_pil
 	w, h = img.size
-	factor_for_label = 1440 * ratio / h if h >= w else 1900 * ratio / w
-	factor_for_button = 480 * ratio / h if h >= w else 400 * ratio / w
-	img_for_label = img.resize(
-		(int(factor_for_label * w), int(factor_for_label * h))
-	)
-	img_for_button = img.resize(
-		(int(factor_for_button * w), int(factor_for_button * h))
-	)
+	# label_size = (int(1440 * ratio * w / h), int(1440 * ratio)) if h >= w else (int(1900 * ratio), int(1900 * ratio * h / w))
+	button_size = (int(480 * ratio * w / h), int(480 * ratio)) if h >= w else (int(400 * ratio), int(400 * ratio * h / w))
+	img_for_label = img
+	# img_for_label = img.resize(label_size)
+	img_for_button = img.resize(button_size)
 	return img_for_label, img_for_button, img_file
 
 
 def encode(_str: str) -> int:
 	num = re.findall('\d+', _str)
-	try:
-		_num_list = [str(int(i)) for i in num]
-		_num_str = ''.join(_num_list)
-		_num = int(_num_str)
-	except IndexError:
-		_num = 0
-	except ValueError:
-		_num = 0
-	return _num
+	if num:
+		return max(0, min(int(''.join(num)), 2147483647))
+	else:
+		return 0
 
 
 def thread_func(func, kwargs=None) -> threading.Thread:
@@ -44,4 +37,19 @@ def thread_func(func, kwargs=None) -> threading.Thread:
 
 def flatten(_list: list) -> list:
 	res = sum(([x] if not isinstance(x, list) else flatten(x) for x in _list), [])
+	return res
+
+
+# From ChatGPT
+def find_longest_common_prefix(lists):
+	if not lists:
+		return []
+
+	it = zip(*lists)
+	res = []
+	for group in it:
+		if all(x == group[0] for x in group):
+			res.append(group[0])
+		else:
+			break
 	return res
