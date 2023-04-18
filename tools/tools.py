ff1f21eb0666f5eea36b81d78ab3488fb2fd2058
Adijnnuuy <96059201+Adi-SOUL@users.noreply.github.com>
2023-04-19 04:21:29 +0800

    Add files via upload

--- tools/tools.py
+++ tools/tools.py
@@ -1,22 +1,44 @@
 import re
 import threading
 from PIL import Image
-
+from cn_clip.clip import load_from_name
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 Image.MAX_IMAGE_PIXELS = None
-from typing import Tuple
+from typing import Tuple, List
+from torch.cuda import is_available
+from torch import no_grad
+from PIL import Image
+
+from cn_clip import clip
 
 
-def get_img(img_file: str, ratio: float, img_pil: Image = None) -> Tuple[Image.Image, Image.Image, str]:
+def get_img(
+		img_file: str,
+		ratio: float,
+		full_size=False,
+		img_pil: Image = None
+) -> Tuple[None, None, None] | Tuple[Image.Image, Image.Image, str]:
 	if img_pil is None:
-		img = Image.open(img_file)
+		try:
+			img = Image.open(img_file)
+		except FileNotFoundError:
+			return None, None, None
 	else:
 		img = img_pil
 	w, h = img.size
-	# label_size = (int(1440 * ratio * w / h), int(1440 * ratio)) if h >= w else (int(1900 * ratio), int(1900 * ratio * h / w))
-	button_size = (int(480 * ratio * w / h), int(480 * ratio)) if h >= w else (int(400 * ratio), int(400 * ratio * h / w))
-	img_for_label = img
-	# img_for_label = img.resize(label_size)
+	button_size = (
+		int(480 * ratio * w / h),
+		int(480 * ratio)
+	) if h >= w else (
+		int(400 * ratio),
+		int(400 * ratio * h / w)
+	)
+	factor_for_label = 1440 * ratio / h if h >= w else 1900 * ratio / w
 	img_for_button = img.resize(button_size)
+	img_for_label = img.resize(
+		(int(factor_for_label * w), int(factor_for_label * h))
+	) if not full_size else img
 	return img_for_label, img_for_button, img_file
 
 
@@ -53,3 +75,25 @@ def find_longest_common_prefix(lists):
 		else:
 			break
 	return res
+
+
+def clip_sort(files: List[str], text: str) -> List[str]:
+	device = "cuda" if is_available() else "cpu"
+	model, preprocess = load_from_name("ViT-L-14", device=device, download_root='./')
+	model.eval()
+	_text = clip.tokenize([text]).to(device)
+	res_dict = dict()
+	with no_grad():
+		for img_file in files:
+			image = preprocess(Image.open(img_file)).unsqueeze(0).to(device)
+			image_features = model.encode_image(image)
+			text_features = model.encode_text(_text)
+			image_features /= image_features.norm(dim=-1, keepdim=True)
+			text_features /= text_features.norm(dim=-1, keepdim=True)
+			similarity = text_features.cpu().numpy() @ image_features.cpu().numpy().T
+			similarity = similarity[0][0]
+			res_dict[img_file] = similarity
+
+	files.sort(key=lambda img_name: res_dict[img_name])
+	files.reverse()
+	return files
